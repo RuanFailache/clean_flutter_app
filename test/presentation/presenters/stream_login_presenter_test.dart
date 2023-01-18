7fc470ea32840ab6edaa9aae66c4dab4bed5f232
Ruan Failache <ruanfailache@gmail.com>
2023-01-17 22:36:50 -0300

    ensure StreamLoginPresenter calls Validation on email changed

---
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -0,0 +1,40 @@
+import 'package:faker/faker.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+abstract class Validation {
+  void validate({
+    required String field,
+    required String value,
+  });
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  StreamLoginPresenter({required this.validation});
+
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+class MockValidation extends Mock implements Validation {}
+
+void main() {
+  test(
+    'Should call Validation with correct email',
+    () async {
+      final validation = MockValidation();
+      final sut = StreamLoginPresenter(validation: validation);
+      final email = faker.internet.email();
+      sut.validateEmail(email);
+      verify(
+        validation.validate(
+          field: 'email',
+          value: email,
+        ),
+      ).called(1);
+    },
+  );
+}
