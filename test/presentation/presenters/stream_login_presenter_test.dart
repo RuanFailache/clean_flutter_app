3f5d774a6121d2826f9266c8e96e1cb30f9ae831
Ruan Failache <ruanfailache@gmail.com>
2023-01-19 13:19:36 -0300

    move Validation to its own file

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,18 +1,12 @@
 import 'dart:async';
 
 import 'package:faker/faker.dart';
+import 'package:for_dev/presentation/dependencies/dependencies.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
 import 'stream_login_presenter_test.mocks.dart';
-
-abstract class Validation {
-  String? validate({
-    required String field,
-    required String value,
-  });
-}
 
 class LoginState {
   String? emailError;
